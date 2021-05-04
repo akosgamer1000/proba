06650a4f548f625a7fd79418b62e9359a602a11a
akosgamer1000 <75310512+akosgamer1000@users.noreply.github.com>
2021-05-04 11:39:54 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function table() {
+ let body=document.body;
+ let  table=document.createElement(table)
+ let row = table.insertRow(j);
+ let cell=row.insertCell(0);
+ 
+}
+
+table();
---
+++ codep.js
@@ -0,0 +1,2 @@
+let cell1=document.getElementById("1")
+cell1.innerText="<p>1<p>";
